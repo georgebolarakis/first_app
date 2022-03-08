078258482cb64963860dd07989d589003910dcc4
georgelabrys <100436759+georgelabrys@users.noreply.github.com>
2022-03-08 10:08:10 +0000

    some modifictions

--- Complete_Guide/flutter_complete_guide/lib/main.dart
+++ Complete_Guide/flutter_complete_guide/lib/main.dart
@@ -4,10 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
+//another way to wright the above code, when you just have one argument
+//void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
-  build(BuildContext context) {
+  //adding a decorator to make the code cleaner since we are using out widget instead of the StatelessWidget
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first App"),
+        ),
+        body: Text("This is my default text!"),
+      ),
     );
   }
 }
